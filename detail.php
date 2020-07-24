2a02a44ba007323c3c446199c55b70efb4cc2ec6
ruderr <ruderr@gmail.com>
2020-07-23 23:00:31 -0300

    listo para probar heroku 4

--- detail.php
+++ detail.php
@@ -30,9 +30,12 @@ $payer->email = "test_user_63274575@testuser.com";
 $payer->date_created = "2018-06-02T12:58:41.425-04:00";
 $payer->phone = array(
   "area_code" => "11",
-  "number" => ": 22223333"
+  "number" => "22223333"
 );
-
+$payer->identification = array(
+  "type" => "DNI",
+  "number" => "12345678"
+);
 $payer->address = array(
   "street_name" => "False",
   "street_number" => 123,
